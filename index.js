9cec3db602943a81b710dd22c3e42232c18c487b
sophia <sophiendalo@gmail.com>
2022-11-23 11:06:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(Feet){
+  if (Feet <= 400){
+  return"This one is on me!"
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
+ else if (Feet >2000 && Feet < 2500){
+return "I will gladly take your thirty bucks."
+}return "No can do.";
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city ==='NYC'?"Ok, sounds good." :"No go."
+}
+  
+function switchOnCharmFromTip(G){
+  switch(G){
+  case "generous":
+    return "Thank you so much."
+    case "not as generous":
+    return 'Thank you.'
+    default :
+    return 'Bye.'
+  }
 }
